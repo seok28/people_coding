f1cc1992c686e493cd01c79d14c923af1ee0cc05
seok <seokjoonsoo@gmail.com>
2022-09-05 18:09:50 +0900

    first commit

---
+++ app/src/main/java/com/example/comin/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.comin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
+
+class MainActivity : AppCompatActivity() {
+
+    internal lateinit var viewpager: ViewPager
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        viewpager = findViewById(R.id.viewpager) as ViewPager
+
+        val adapter = ViewPagerAdapter(this)
+        viewpager.adapter = adapter
+
+    }
+}
